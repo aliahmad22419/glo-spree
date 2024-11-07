64cdf0166b2081035f40fe251cca0f6dfd240b51
ali ahmad <ali.ahmad@sabre.com>
2024-11-07 14:10:53 +0500

    Added controller to check CSS-javaScript-images and run the project with rails and Esbuild

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript is working!');
+    // debugger;
+
+    const img = new Image();
+    img.src = '/assets/cap.jpg';
+
+    img.onload = function() {
+        console.log('Image loaded succesxxsfully!');
+    };
+
+    img.onerror = function() {
+        console.log('Error loading image');
+    };
+});
+
