3e2f059de7db8b74e4872a558a7db098164c8089
ali ahmad <ali.ahmad@sabre.com>
2024-12-17 14:44:13 +0500

    Added test/models in to Assets.

---
+++ test/controllers/spree/api/v2/storefront/reports_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Spree::Api::V2::Storefront::ReportsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/vendor_sale_analysis_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class VendorSaleAnalysisTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
